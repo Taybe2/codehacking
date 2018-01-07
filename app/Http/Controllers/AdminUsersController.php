c2a77829213c3a2da767418df07945a0530c7f11
Taybe <taybemuharem@gmail.com>
2018-01-07 10:22:28 +0000

    Uploading photos is working.

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -8,6 +8,7 @@ use App\Http\Requests;
 
 use App\User;
 use App\Role;
+use App\Photo;
 use App\Http\Requests\UsersRequest;
 
 class AdminUsersController extends Controller
@@ -49,9 +50,25 @@ class AdminUsersController extends Controller
     public function store(UsersRequest $request)
     {
         //
-        User::create($request->all());
+        $input = $request->all();
         
-        return redirect('admin/users');
+        if($file = $request->file('photo_id')){
+            
+            $name = time() . $file->getClientOriginalName();
+            
+            $file->move('images', $name);
+            
+            $photo = Photo::create(['file'=>$name]);
+            
+            $input['photo_id'] = $photo->id;
+            
+        }
+        
+        $input['password'] = bcrypt($request->password);
+        
+        User::create($input);
+        
+        //return redirect('admin/users');
         
     }
 
